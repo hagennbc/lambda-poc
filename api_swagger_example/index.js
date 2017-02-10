e460a63f32376b7ac22b5fd3aa269fe0fc82982a
Hagen Dias <hagen.dias@nbcuni.com>
2017-02-10 15:10:46 -0500

    udating for prod example

--- api_swagger_example/index.js
+++ api_swagger_example/index.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
 
     callback(null, {
         statusCode: '200',
-        body: "Hello world dev 2.0"
+        body: "Hello world prod 3.0"
     });
 
 }
